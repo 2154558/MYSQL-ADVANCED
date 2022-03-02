fe5f183831b057e2f63cc14d7ca2307948b9ef0e
2154558 <90393278+2154558@users.noreply.github.com>
2022-03-02 11:04:13 +0100

    opgave 01 af

---
+++ antwoorden.sql
@@ -0,0 +1,7 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (123, 'jan', 'klaas', '2c', 'wonerplats', '4444CC', 2000-01-02, 'amsterdam');
+VALUES (124, 'jan', 'klaas', '2c', 'wonerplats', '4444CC', 2000-01-02, 'amsterdam');
+VALUES (125, 'jan', 'klaas', '2c', 'wonerplats', '4444CC', 2000-01-02, 'amsterdam');
+VALUES (126, 'jan', 'klaas', '2c', 'wonerplats', '4444CC', 2000-01-02, 'amsterdam');
+VALUES (127, 'jan', 'klaas', '2c', 'wonerplats', '4444CC', 2000-01-02, 'amsterdam');
+VALUES (128, 'jan', 'klaas', '2c', 'wonerplats', '4444CC', 2000-01-02, 'amsterdam');
