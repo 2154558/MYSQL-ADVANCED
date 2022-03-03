757dd9bf70561166eb607a463fe83a498a7a159f
2154558 <90393278+2154558@users.noreply.github.com>
2022-03-03 12:48:19 +0100

    opgave 3 af

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,24 @@
 -- Opdracht 1
-
+SELECT woonplaats FROM studenten; 
 -- Opdracht 2 
-
+INSERT INTO studenten(adres, postcode)
+VALUES ('wonerplats', '4444CC')
+WHERE student_id = 8, 9, 10;
 -- Opdracht 3
-
+UPDATE studenten2
+SET geboortedatum = 2000-12-12
+WHERE voornaam = 'Marloes';
 -- Opdracht 4
-
+UPDATE studenten2
+SET klas = '9A'
+WHERE voornaam = 'Mohammed';
+SET klas = '9C'
+WHERE voornaam = 'Jan';
 -- Opdracht 5
+UPDATE studenten2
+SET naam = 'Johanna'
+WHERE voornaam = 'Johannah';
+
+
+
+
